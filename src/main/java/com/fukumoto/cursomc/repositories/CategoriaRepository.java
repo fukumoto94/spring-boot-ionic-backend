a2b49eba3d775d44ad09e47e998a8de9675e4675
Lucas Fukumoto <lucas.fukumoto@hotmail.com>
2018-06-23 13:54:45 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/fukumoto/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.fukumoto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fukumoto.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
