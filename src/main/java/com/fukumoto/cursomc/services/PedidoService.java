6fcff341f4fb2016acaf9306ea0429b76e506152
Lucas Fukumoto <lucas.fukumoto@hotmail.com>
2018-06-24 22:48:06 -0300

    Endpoint/ pedidos/ {id} disponibilizado

---
+++ src/main/java/com/fukumoto/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.fukumoto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fukumoto.cursomc.domain.Pedido;
+import com.fukumoto.cursomc.repositories.PedidoRepository;
+import com.fukumoto.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado Id:"+id+", Tipo: "+Pedido.class.getName())) ;
+	}
+	
+}
